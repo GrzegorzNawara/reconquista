fbc3dca9e3a3a186f93988e313077ebdaffe5048
Grzegorz Nawara <grzegorz.nawara@gmail.com>
2018-02-15 11:55:26 +0100

    test

--- api_claim_role.php
+++ api_claim_role.php
@@ -10,27 +10,30 @@ error
 // FUNCTION
 Read user data
 
+
+
+
 */
 
 
 // Report simple running errors
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
-	
-	
+
+
 	$game_id=$_GET["game_id"];
 	$user_id=$_GET["user_id"];
 	$role_id=$_GET["role_id"];
-	
+
 
 	include('inc_game.php');
-	
-	
+
+
 	$status_array=_read_($game_id, $role_id);
 	$status_array[STATUS_USER_ID]=$user_id;
 	//_write_($session['game_id'], $session['player_id'], $status_array);
 	_write_($game_id, $role_id, $status_array);
-	
-	
+
+
 	$return_data=array();
 	$return_data['error']='';
 
